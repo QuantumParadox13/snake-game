55d3398a4e1ce3c84204eb68490007694a759f34
Maxine <arg100@gmail.com>
2021-05-13 14:46:55 -0400

    Test t.setOpacity(float)

--- Transparent.java
+++ Transparent.java
@@ -8,16 +8,15 @@ public class Transparent extends JComponent{
     public static void createTransparentWindow(){
         JFrame t = new JFrame("transparent");
         t.setSize(1000, 500);
-        t.setVisible(true);
         // t.getContentPane().setOpaque(false);
         ((JComponent) t.getContentPane()).setOpaque(true);
+        // t.setOpacity(0.8f);
         t.getContentPane().setBackground(new Color(0, 0, 0, 0));
         t.addWindowListener(new WindowAdapter() {// when window closes, exit program
             public void windowClosing(WindowEvent windowEvent) {
                 System.exit(0);
             }
         });
-        t.getRootPane().putClientProperty("Window.alpha", 0.8f);
-
+        t.setVisible(true);
     }
 }
